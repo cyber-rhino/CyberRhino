ff73ddaa0eb6efaff38db3fb29f9fb4ce1cb95a1
JoaquimAlt <joaquimferreira1818@gmail.com>
2022-05-21 13:07:08 -0300

    first commit home, nav, footer

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import "./assets/styles/custom.scss";
+import "bootstrap/js/src/collapse.js";
+import Navbar from "./sections/Navbar";
+import Home from "./sections/Home";
+import Footer from "./sections/Footer";
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Home />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
