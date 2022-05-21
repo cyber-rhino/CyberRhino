f6df3421516009434e2dd5c6faefc8e2941fbc0e
JoaquimAlt <joaquimferreira1818@gmail.com>
2022-05-21 17:06:29 -0300

    organizacao components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./App.css";
 import "./assets/styles/custom.scss";
 import "bootstrap/js/src/collapse.js";
-import Navbar from "./sections/Navbar";
+import Navbar from "./components/Navbar";
 import Home from "./sections/Home";
 import Footer from "./sections/Footer";
 
 function App() {
   return (
     <>
+    <div className="wrapper">
     <Navbar />
     <Home />
+    </div>
     <Footer />
     </>
   );
