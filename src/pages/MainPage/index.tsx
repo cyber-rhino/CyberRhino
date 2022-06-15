bb74cdcc45aaae279926a31bd3d6901016559283
JoaquimFS <63430363+JoaquimAlt@users.noreply.github.com>
2022-06-14 21:36:34 -0300

    update backtotop icon

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -15,39 +15,29 @@ function MainPage() {
         <Contact />
 
         <a id="backToTopButton" href="#navigation">
-          <svg
-            width="125"
-            height="125"
-            viewBox="0 0 125 125"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle
-              cx="62.5"
-              cy="62.5"
-              r="61"
-              fill="url(#paint0_linear_497_7)"
-              stroke="white"
-              stroke-width="3"
-            />
-            <path
-              d="M58.75 87.5C58.75 89.5711 60.4289 91.25 62.5 91.25C64.5711 91.25 66.25 89.5711 66.25 87.5L58.75 87.5ZM65.1517 37.3483C63.6872 35.8839 61.3128 35.8839 59.8483 37.3483L35.9835 61.2132C34.519 62.6777 34.519 65.052 35.9835 66.5165C37.448 67.981 39.8223 67.981 41.2868 66.5165L62.5 45.3033L83.7132 66.5165C85.1777 67.981 87.552 67.981 89.0165 66.5165C90.481 65.052 90.481 62.6777 89.0165 61.2132L65.1517 37.3483ZM66.25 87.5L66.25 40L58.75 40L58.75 87.5L66.25 87.5Z"
-              fill="white"
-            />
-            <defs>
-              <linearGradient
-                id="paint0_linear_497_7"
-                x1="62.5"
-                y1="6.98855e-10"
-                x2="63"
-                y2="125"
-                gradientUnits="userSpaceOnUse"
-              >
-                <stop stop-color="#15BFE3" />
-                <stop offset="1" stop-color="#1523EC" />
-              </linearGradient>
-            </defs>
-          </svg>
+          <svg width="151" height="151" viewBox="0 0 151 151" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g filter="url(#filter0_d_497_7)">
+<circle cx="75.5" cy="63.5" r="61" fill="url(#paint0_linear_497_7)" stroke="white" stroke-width="3"/>
+<path d="M71.75 88.5C71.75 90.5711 73.4289 92.25 75.5 92.25C77.5711 92.25 79.25 90.5711 79.25 88.5L71.75 88.5ZM78.1517 38.3483C76.6872 36.8839 74.3128 36.8839 72.8483 38.3483L48.9835 62.2132C47.519 63.6777 47.519 66.052 48.9835 67.5165C50.448 68.981 52.8223 68.981 54.2868 67.5165L75.5 46.3033L96.7132 67.5165C98.1777 68.981 100.552 68.981 102.017 67.5165C103.481 66.052 103.481 63.6777 102.017 62.2132L78.1517 38.3483ZM79.25 88.5L79.25 41L71.75 41L71.75 88.5L79.25 88.5Z" fill="white"/>
+</g>
+<defs>
+<filter id="filter0_d_497_7" x="0" y="0" width="151" height="151" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
+<feOffset dy="12"/>
+<feGaussianBlur stdDeviation="6.5"/>
+<feComposite in2="hardAlpha" operator="out"/>
+<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_497_7"/>
+<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_497_7" result="shape"/>
+</filter>
+<linearGradient id="paint0_linear_497_7" x1="75.5" y1="1" x2="76" y2="126" gradientUnits="userSpaceOnUse">
+<stop stop-color="#15BFE3"/>
+<stop offset="1" stop-color="#1523EC"/>
+</linearGradient>
+</defs>
+</svg>
+
         </a>
       </body>
     </>
